ef7c470a96e95c2965802e837b64eb5ca8d38193
AtomicLiquors <gyqls234@gmail.com>
2023-08-23 11:06:10 +0900

    codingapple 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const title = 'Suicide Squad'
+  const id = 'head'
+  let [titles, useTitles] = useState(['콩이', '구름이', '시아님']);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 id={id} style={{color: 'gray', fontSize: '16px'}}>블로그</h4>
+      </div>
+      <div className = "list">
+        <h4>{titles[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{titles[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{titles[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4>{title}</h4>
     </div>
   );
 }
