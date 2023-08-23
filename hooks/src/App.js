ebd2c7244beac3eaa3c217fbfe1d29302b8e0b57
chb <gyqls234@gmail.com>
2023-08-24 00:21:58 +0900

    useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,20 +2,35 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
-  const [time, setTime] = useState(1);
-  const modifyTime = () => {
-    let newTime;
+const heavyWork = () => {
+  console.log('big load of work');
+  return ['이콩이', '구름이'];
+}
 
-    newTime = (time >= 12 ? 1 : time + 1);
-    
-    setTime(newTime)
-  } 
+function App() {
+  //const [names, setNames] = useState(heavyWork);
+  const [names, setNames] = useState(() => {
+   return heavyWork();
+  });
+  const [input, setInput] = useState('');
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      console.log(prevState);
+      return([input, ...prevState])
+    })    
+  };
 
   return (
     <div className="App">
-      <span>현재시각 : {time}시</span>
-      <button onClick={ modifyTime }>Update</button>
+      <input type = "text" value={input} onChange={handleInputChange}/>
+      <button onClick={handleUpload}>Upload</button>
+      {names.map( (name, idx) => {
+        return <p key = {idx}>{name}</p>
+      })}
     </div>
   );
 }
