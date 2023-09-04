b5ffbc177971c7c79ca470ef40a6966381d4fdfe
AtomicLiquors <gyqls234@gmail.com>
2023-09-04 13:15:16 +0900

    -

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
+import About from "./pages/About.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
       <Routes>
         <Route path="/" element={<App/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
       </Routes>
     </Router>
   </React.StrictMode>
